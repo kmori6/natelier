6d79f836e2270bfdfcc95d813a52532cf48af81a
morik <kosuke.mori.5120@gmail.com>
2022-12-10 06:59:03 +0900

    inherit transformer model

--- models/bart.py
+++ models/bart.py
@@ -1,10 +1,8 @@
-from typing import Tuple, Dict, Any
-from tqdm import tqdm
 import torch
 import torch.nn as nn
-from .modules.learnable_embedding import LearnableEmbedding
-from .modules.multi_head_attention import MultiHeadAttention
-from .modules.feed_forward import FeedForward
+from tqdm import tqdm
+
+from .transformer import Decoder, Embedding, Encoder, TransformerModel
 
 KEY_DICT = {
     "encoder": {
@@ -69,71 +67,45 @@ KEY_DICT = {
 }
 
 
-class EncoderLayer(nn.Module):
+class LearnableEmbedding(Embedding):
     def __init__(
-        self, d_model: int, d_ff: int, num_attention_heads: int, dropout_rate: float
-    ):
-        super().__init__()
-        self.mha = MultiHeadAttention(d_model, num_attention_heads, dropout_rate)
-        self.ff = FeedForward(d_model, d_ff)
-        self.mha_norm = nn.LayerNorm(d_model)
-        self.ff_norm = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(dropout_rate)
-
-    def forward(
-        self, hs: torch.Tensor, masks: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        # multi-head attention
-        shortcut = hs
-        hs = self.dropout(self.mha(hs, hs, hs, masks))
-        hs = self.mha_norm(shortcut + hs)
-        # feed-forward
-        shortcut = hs
-        hs = self.dropout(self.ff(hs))
-        hs = self.ff_norm(shortcut + hs)
-        return hs, masks
-
-
-class DecoderLayer(nn.Module):
-    def __init__(
-        self, d_model: int, d_ff: int, num_attention_heads: int, dropout_rate: float
-    ):
-        super().__init__()
-        self.masked_mha = MultiHeadAttention(d_model, num_attention_heads, dropout_rate)
-        self.mha = MultiHeadAttention(d_model, num_attention_heads, dropout_rate)
-        self.ff = FeedForward(d_model, d_ff)
-        self.masked_mha_norm = nn.LayerNorm(d_model)
-        self.mha_norm = nn.LayerNorm(d_model)
-        self.ff_norm = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(dropout_rate)
-
-    def forward(
         self,
-        decoder_hs: torch.Tensor,
-        decoder_masks: torch.Tensor,
-        encoder_hs: torch.Tensor,
-        encoder_masks: torch.Tensor,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        # masked multi-head attention
-        shortcut = decoder_hs
-        decoder_hs = self.dropout(
-            self.masked_mha(decoder_hs, decoder_hs, decoder_hs, decoder_masks)
-        )
-        decoder_hs = self.masked_mha_norm(shortcut + decoder_hs)
-        # multi-head attention
-        shortcut = decoder_hs
-        decoder_hs = self.dropout(
-            self.mha(decoder_hs, encoder_hs, encoder_hs, encoder_masks)
-        )
-        decoder_hs = self.mha_norm(shortcut + decoder_hs)
-        # feed-forward
-        shortcut = decoder_hs
-        decoder_hs = self.dropout(self.ff(decoder_hs))
-        decoder_hs = self.ff_norm(shortcut + decoder_hs)
-        return decoder_hs, decoder_masks, encoder_hs, encoder_masks
+        vocab_size: int,
+        position_size: int,
+        d_model: int,
+        dropout_rate: float,
+        padding_id: int,
+        token_embedding: nn.Embedding,
+    ):
+        super().__init__(vocab_size, d_model, padding_id, dropout_rate)
+        self.padding_id = padding_id
+        self.token_embedding = token_embedding
+        self.position_embedding = nn.Embedding(position_size, d_model, padding_id)
+        self.embedding_norm = nn.LayerNorm(d_model)
+
+    def forward(self, tokens: torch.Tensor) -> torch.Tensor:
+        token_embedding = self.token_embedding(tokens)
+        position_embedding = self.embed_positions(tokens)
+        hs = self.embedding_norm(token_embedding + position_embedding)
+        hs = self.dropout(hs)
+        return hs
+
+    def embed_positions(self, tokens: torch.Tensor) -> torch.Tensor:
+        batch_size, length = tokens.size()
+        padding_mask = tokens != self.padding_id
+        # tokens: [[11, 12, 1, 1], [11, 12, 13, 14]] (padding_id = 1)
+        # positions: [[2, 3, 1, 1], [2, 3, 4, 5]]
+        positions = torch.arange(
+            self.padding_id,
+            length + self.padding_id,
+            dtype=torch.long,
+            device=tokens.device,
+        ).repeat(batch_size, 1)
+        positions[padding_mask] = self.padding_id
+        return self.position_embedding(positions)
 
 
-class Encoder(nn.Module):
+class Encoder(Encoder):
     def __init__(
         self,
         vocab_size: int,
@@ -144,9 +116,19 @@ class Encoder(nn.Module):
         num_layers: int,
         dropout_rate: float,
         padding_id: int,
+        ff_activation: nn.Module,
         input_embedding: nn.Embedding,
     ):
-        super().__init__()
+        super().__init__(
+            vocab_size=vocab_size,
+            d_model=d_model,
+            d_ff=d_ff,
+            num_attention_heads=num_attention_heads,
+            num_layers=num_layers,
+            dropout_rate=dropout_rate,
+            padding_id=padding_id,
+            ff_activation=ff_activation,
+        )
         self.embedding = LearnableEmbedding(
             vocab_size=vocab_size,
             position_size=position_size,
@@ -155,23 +137,9 @@ class Encoder(nn.Module):
             padding_id=padding_id,
             token_embedding=input_embedding,
         )
-        self.layers = nn.ModuleList(
-            [
-                EncoderLayer(d_model, d_ff, num_attention_heads, dropout_rate)
-                for _ in range(num_layers)
-            ]
-        )
-
-    def forward(
-        self, tokens: torch.Tensor, masks: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        hs = self.embedding(tokens)
-        for layer in self.layers:
-            hs, masks = layer(hs, masks)
-        return hs, masks
 
 
-class Decoder(nn.Module):
+class Decoder(Decoder):
     def __init__(
         self,
         vocab_size: int,
@@ -182,9 +150,19 @@ class Decoder(nn.Module):
         num_layers: int,
         dropout_rate: float,
         padding_id: int,
+        ff_activation: nn.Module,
         output_embedding: nn.Embedding,
     ):
-        super().__init__()
+        super().__init__(
+            vocab_size=vocab_size,
+            d_model=d_model,
+            d_ff=d_ff,
+            num_attention_heads=num_attention_heads,
+            num_layers=num_layers,
+            dropout_rate=dropout_rate,
+            padding_id=padding_id,
+            ff_activation=ff_activation,
+        )
         self.embedding = LearnableEmbedding(
             vocab_size=vocab_size,
             position_size=position_size,
@@ -193,32 +171,11 @@ class Decoder(nn.Module):
             padding_id=padding_id,
             token_embedding=output_embedding,
         )
-        self.layers = nn.ModuleList(
-            [
-                DecoderLayer(d_model, d_ff, num_attention_heads, dropout_rate)
-                for _ in range(num_layers)
-            ]
-        )
         self.classifier = nn.Linear(d_model, vocab_size, bias=False)
         self.classifier.weight = output_embedding.weight
 
-    def forward(
-        self,
-        decoder_tokens: torch.Tensor,
-        decoder_masks: torch.Tensor,
-        encoder_hs: torch.Tensor,
-        encoder_masks: torch.Tensor,
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        hs = self.embedding(decoder_tokens)
-        for layer in self.layers:
-            hs, decoder_masks, encoder_hs, encoder_masks = layer(
-                hs, decoder_masks, encoder_hs, encoder_masks
-            )
-        hs = self.classifier(hs)
-        return hs
 
-
-class BartModel(nn.Module):
+class BartModel(TransformerModel):
     def __init__(
         self,
         vocab_size: int = 51201,
@@ -232,12 +189,18 @@ class BartModel(nn.Module):
         eos_id: int = 2,
         padding_id: int = 1,
     ):
-        super().__init__()
-        self.vocab_size = vocab_size
-        self.d_model = d_model
-        self.bos_id = bos_id
-        self.eos_id = eos_id
-        self.padding_id = padding_id
+        super().__init__(
+            vocab_size=vocab_size,
+            d_model=d_model,
+            d_ff=d_ff,
+            num_attention_heads=num_attention_heads,
+            num_encoder_layers=num_layers,
+            num_decoder_layers=num_layers,
+            dropout_rate=dropout_rate,
+            padding_id=padding_id,
+            bos_id=bos_id,
+            eos_id=eos_id,
+        )
         common_token_embedding = nn.Embedding(vocab_size, d_model, padding_id)
         self.encoder = Encoder(
             vocab_size=vocab_size,
@@ -248,6 +211,7 @@ class BartModel(nn.Module):
             num_layers=num_layers,
             dropout_rate=dropout_rate,
             padding_id=padding_id,
+            ff_activation=nn.GELU(),
             input_embedding=common_token_embedding,
         )
         self.decoder = Decoder(
@@ -259,6 +223,7 @@ class BartModel(nn.Module):
             num_layers=num_layers,
             dropout_rate=dropout_rate,
             padding_id=padding_id,
+            ff_activation=nn.GELU(),
             output_embedding=common_token_embedding,
         )
 
@@ -268,82 +233,6 @@ class BartModel(nn.Module):
         self.decoder.embedding.token_embedding = common_token_embedding
         self.decoder.classifier = nn.Linear(self.d_model, vocab_size, bias=False)
         self.decoder.classifier.weight = common_token_embedding.weight
-
-    def forward(
-        self,
-        encoder_tokens: torch.Tensor,
-        encoder_masks: torch.Tensor,
-        decoder_tokens: torch.Tensor,
-        decoder_masks: torch.Tensor,
-    ):
-        encoder_hs, encoder_masks = self.encoder(encoder_tokens, encoder_masks)
-        logits = self.decoder(decoder_tokens, decoder_masks, encoder_hs, encoder_masks)
-        return logits
-
-    def decode(
-        self,
-        tokens: torch.Tensor,
-        beam_size: int = 5,
-        max_length: int = 128,
-    ) -> Dict[str, Any]:
-
-        # initial stats
-        running_stats = [{"score": 0.0, "tokens": [self.bos_id]}]
-        final_stats = []
-
-        # encoder forward
-        encoder_inputs = tokens.repeat_interleave(beam_size, dim=0)
-        encoder_masks = torch.ones_like(encoder_inputs)
-        encoder_hs, encoder_masks = self.encoder(encoder_inputs, encoder_masks)
-
-        # beam search
-        for i in range(1, max_length):
-
-            # decoder forward
-            decoder_tokens = tokens.new_zeros(beam_size, i)
-            for beam_idx, stat in enumerate(running_stats):
-                decoder_tokens[beam_idx, :] = tokens.new_tensor(stat["tokens"])
-            decoder_masks = decoder_tokens.new_ones(beam_size, i, i).tril()
-            logits = self.decoder(
-                decoder_tokens, decoder_masks, encoder_hs, encoder_masks
-            )
-            next_token_scores = torch.log_softmax(logits[:, -1, :], dim=-1)  # (B, D)
-
-            # scoring
-            aggregator = []
-            for beam_idx, stat in enumerate(running_stats):
-                next_scores, next_tokens = torch.topk(
-                    next_token_scores[beam_idx], beam_size
-                )
-                for topk_idx in range(beam_size):
-                    candidate = {
-                        "score": stat["score"] + next_scores[topk_idx].item(),
-                        "tokens": stat["tokens"] + [next_tokens[topk_idx].item()],
-                    }
-                    aggregator.append(candidate)
-            running_stats = sorted(aggregator, key=lambda x: x["score"], reverse=True)[
-                :beam_size
-            ]
-
-            # add eos_token_id
-            if i == max_length - 1:
-                for stat in running_stats:
-                    stat["tokens"].append(self.eos_id)
-
-            # sort stats
-            keep_stats = []
-            for stat in running_stats:
-                if stat["tokens"][-1] == self.eos_id:
-                    final_stats.append(stat)
-                else:
-                    keep_stats.append(stat)
-            running_stats = keep_stats
-
-            # stop search
-            if len(running_stats) < 1 or len(final_stats) >= beam_size:
-                break
-
-        return sorted(final_stats, key=lambda x: x["score"], reverse=True)[0]
 
     @classmethod
     def from_pretrained(cls):
