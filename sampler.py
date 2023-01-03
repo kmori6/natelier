886e0ff55041e3a92e8bc35a789c3b95a1d49a29
morik <kosuke.mori.5120@gmail.com>
2023-01-03 09:46:24 +0900

    update the sampler

--- sampler.py
+++ sampler.py
@@ -1,31 +1,20 @@
 import random
-from typing import Iterable, Iterator, List
+from typing import Iterator, List
 
+import numpy as np
 from torch.utils.data import Dataset
 
 
-class DefaultSampler:
-    def __init__(self, dataset: Dataset, shuffle: bool):
-        self.dataset = dataset
-        self.shuffle = shuffle
-
-    def __len__(self) -> int:
-        return len(self.dataset)
-
-    def __iter__(self) -> Iterator[int]:
-        idx_list = list(range(len(self.dataset)))
-        if self.shuffle:
-            random.shuffle(idx_list)
-        return iter(idx_list)
-
-
 class DefaultBatchSampler:
-    def __init__(self, sampler: Iterable[int], batch_size: int, drop_last: bool):
-        self.sampler = sampler
+    def __init__(
+        self, dataset: Dataset, batch_size: int, shuffle: bool, drop_last: bool
+    ):
+        self.dataset = dataset
         self.batch_size = batch_size
+        self.shuffle = shuffle
         self.drop_last = drop_last
-        self.num_full_batches = len(sampler) // batch_size
-        self.num_remainder = 0 if drop_last else len(sampler) % batch_size
+        self.num_full_batches = len(dataset) // batch_size
+        self.num_remainder = 0 if drop_last else len(dataset) % batch_size
 
     def __len__(self) -> int:
         if self.num_remainder == 0:
@@ -34,8 +23,13 @@ class DefaultBatchSampler:
             return self.num_full_batches + 1
 
     def __iter__(self) -> Iterator[List[int]]:
-        iterator = iter(self.sampler)
-        for _ in range(self.num_full_batches):
-            yield [next(iterator) for _ in range(self.batch_size)]
-        if self.num_remainder > 0:
-            yield [next(iterator) for _ in range(self.num_remainder)]
+        indices = np.arange(len(self.dataset))
+        if self.shuffle:
+            random.shuffle(indices)
+        indices = np.split(
+            indices, np.arange(self.batch_size, len(indices), self.batch_size)
+        )
+        if self.drop_last and len(self.dataset) % self.batch_size > 0:
+            indices = indices[:-1]
+        iterator = iter([array.tolist() for array in indices])
+        return iterator
