41fd8e2b563f415992ff7ed48774e64c80995009
morik <kosuke.mori.5120@gmail.com>
2023-01-05 21:12:10 +0900

    add the default transformer model

--- tasks/machine-translation/model.py
+++ tasks/machine-translation/model.py
@@ -5,8 +5,29 @@ import torch
 
 from loss.cross_entropy import CrossEntropyLoss
 from metrics import tokens_accuracy
-from models.mbart import Mbart
+from models import Mbart, Transformer
 from outputs import ModelOutputs
+
+
+class NMTTransformer(Transformer):
+    def __init__(self, args: Namespace):
+        super().__init__(args.vocab_size)
+        self.loss_fn = CrossEntropyLoss(label_smoothing=args.label_smoothing)
+
+    def forward(
+        self,
+        encoder_tokens: torch.Tensor,
+        encoder_masks: torch.Tensor,
+        decoder_tokens: torch.Tensor,
+        decoder_masks: torch.Tensor,
+        labels: torch.Tensor,
+    ) -> Dict[str, Any]:
+        logits = super().forward(
+            encoder_tokens, encoder_masks, decoder_tokens, decoder_masks
+        )
+        loss = self.loss_fn(logits.view(-1, self.vocab_size), labels.view(-1))
+        stats = {"loss": loss.item(), "acc": tokens_accuracy(logits.argmax(-1), labels)}
+        return ModelOutputs(loss, stats)
 
 
 class NMTBart(Mbart):
