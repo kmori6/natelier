a8ac627dc7e413479cc66832ff7206259eeb55f0
morik <kosuke.mori.5120@gmail.com>
2023-01-05 21:12:27 +0900

    update the tokenizer

--- tasks/machine-translation/tokenizer.py
+++ tasks/machine-translation/tokenizer.py
@@ -1,7 +1,10 @@
-from typing import List, Dict
+import os
+from typing import Dict, List
+
 import sentencepiece as spm
 import torch
-from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset
+from tqdm import tqdm
 
 
 class NMTTokenizer:
@@ -14,60 +17,50 @@ class NMTTokenizer:
         self.src_lang_id = self.tokenizer.PieceToId(src_lang_token)
         self.tgt_lang_id = self.tokenizer.PieceToId(tgt_lang_token)
 
-    def __call__(self, text_list: List[str], mode: str) -> Dict[str, torch.Tensor]:
-        encodings = {"tokens": [], "masks": []}
-        for text in text_list:
-            if mode == "src":
-                tokens = torch.tensor(self.src_encode(text), dtype=torch.long)
-            elif mode == "tgt":
-                tokens = torch.tensor(self.tgt_encode(text), dtype=torch.long)
-            else:
-                tokens = torch.tensor(self.label_encode(text), dtype=torch.long)
-            encodings["tokens"].append(tokens)
-            encodings["masks"].append(tokens.new_ones(len(tokens)))
-        encodings = {
-            "tokens": pad_sequence(
-                encodings["tokens"],
-                batch_first=True,
-                padding_value=self.pad_token_id,
-            ),
-            "masks": pad_sequence(
-                encodings["masks"], batch_first=True, padding_value=0
-            ),
-        }
-        return encodings
+    def __call__(
+        self, text_source: str, text_target: str = None, **kwargs
+    ) -> Dict[str, torch.Tensor]:
+        src_tokens = self.encode(text_source)
+        results = {"input_ids": torch.tensor([src_tokens], dtype=torch.long)}
+        if text_target:
+            tgt_tokens = self.encode(text_target)
+            results["labels"] = torch.tensor([tgt_tokens], dtype=torch.long)
+        return results
 
-    def src_encode(self, text: str) -> List[int]:
-        return self.tokenizer.EncodeAsIds(text) + [self.eos_token_id, self.src_lang_id]
-
-    def tgt_encode(self, text: str) -> List[int]:
+    def encode(self, text: str) -> List[int]:
         return (
-            [self.tgt_lang_id] + self.tokenizer.EncodeAsIds(text) + [self.eos_token_id]
+            [self.bos_token_id] + self.tokenizer.EncodeAsIds(text) + [self.eos_token_id]
         )
-
-    def label_encode(self, text: str) -> List[int]:
-        return self.tokenizer.EncodeAsIds(text) + [self.eos_token_id, self.tgt_lang_id]
 
     def decode(self, ids: List[int]):
         return self.tokenizer.DecodeIds(ids)
 
     @staticmethod
-    def train_tokenizer(
-        text_file: str,
+    def train(
+        train_dataset: Dataset,
         vocab_size: int,
         out_dir: str,
         src_lang: str,
         tgt_lang: str,
-        bos_token_id: int = 0,
-        pad_token_id: int = 1,
+        pad_token_id: int = 0,
+        bos_token_id: int = 1,
         eos_token_id: int = 2,
         unk_token_id: int = 3,
         model_type: str = "bpe",
     ):
+        os.makedirs(f"{out_dir}/tokenizer", exist_ok=True)
+        text_file = f"{out_dir}/train_text.txt"
+        with open(text_file, "w", encoding="utf-8") as f:
+            for i in tqdm(
+                range(len(train_dataset)), total=len(train_dataset), desc="Loading"
+            ):
+                src_text = train_dataset[i]["src_text"]
+                tgt_text = train_dataset[i]["tgt_text"]
+                f.write(src_text + "\n" + tgt_text + "\n")
         spm.SentencePieceTrainer.train(
             f"--input={text_file} "
             f"--bos_id={bos_token_id} "
-            f"--pad_id={pad_token_id}"
+            f"--pad_id={pad_token_id} "
             f"--eos_id={eos_token_id} "
             f"--unk_id={unk_token_id} "
             f"--model_prefix={out_dir}/tokenizer "
@@ -75,3 +68,4 @@ class NMTTokenizer:
             f"--model_type={model_type} "
             f"--user_defined_symbols=<{src_lang}>,<{tgt_lang}> "
         )
+        os.remove(text_file)
