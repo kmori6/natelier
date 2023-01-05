4e81a991b75a9259b7ec592604123e86595efca5
morik <kosuke.mori.5120@gmail.com>
2023-01-05 21:13:25 +0900

    fix the collate function

--- tasks/machine-translation/collator.py
+++ tasks/machine-translation/collator.py
@@ -5,8 +5,6 @@ import torch
 from torch.nn.utils.rnn import pad_sequence
 from transformers import PreTrainedTokenizerFast
 
-from utils import fill_tokens
-
 
 class NMTBatchCollator:
     def __init__(
@@ -17,7 +15,6 @@ class NMTBatchCollator:
         ignore_token_id: int = -100,
     ):
         self.tokenizer = tokenizer
-        self.max_length = min(args.max_length, tokenizer.model_max_length)
         self.return_test_encodings = return_test_encodings
         self.pad_token_id = tokenizer.pad_token_id
         self.ignore_token_id = ignore_token_id
@@ -27,31 +24,32 @@ class NMTBatchCollator:
             [sample["encoder_tokens"] for sample in batch],
             batch_first=True,
             padding_value=self.pad_token_id,
-        )
-        decoder_tokens = pad_sequence(
-            [sample["decoder_tokens"] for sample in batch],
-            batch_first=True,
-            padding_value=self.pad_token_id,
-        )
+        )  # (B, L1)
         labels = pad_sequence(
             [sample["labels"] for sample in batch],
             batch_first=True,
             padding_value=self.ignore_token_id,
-        )
-        encoder_masks = torch.ones_like(encoder_tokens).masked_fill(
-            encoder_tokens == self.pad_token_id, 0
-        )
-        decoder_masks = torch.ones_like(decoder_tokens).masked_fill(
-            decoder_tokens == self.pad_token_id, 0
-        )
+        )  # (B, L2)
         if self.return_test_encodings:
-            return {
-                "tokens": encodings["input_ids"],
-                "labels": fill_tokens(
-                    encodings["labels"], self.pad_token_id, self.ignore_token_id
-                ),
-            }
+            return {"encoder_tokens": encoder_tokens, "labels": labels}
         else:
+            encoder_masks = torch.ones_like(encoder_tokens).masked_fill(
+                encoder_tokens == self.pad_token_id, 0
+            )  # (B, L1)
+            decoder_tokens = pad_sequence(
+                [sample["decoder_tokens"] for sample in batch],
+                batch_first=True,
+                padding_value=self.pad_token_id,
+            )  # (B, L2)
+            batch_size, tgt_length = decoder_tokens.size()
+            decoder_masks = torch.ones_like(decoder_tokens).masked_fill(
+                decoder_tokens == self.pad_token_id, 0
+            )  # (B, L2)
+            decoder_masks = (
+                decoder_masks.repeat_interleave(tgt_length, dim=0)
+                .view(batch_size, tgt_length, tgt_length)
+                .tril()
+            )  # (B, L2, L2)
             return {
                 "encoder_tokens": encoder_tokens,
                 "encoder_masks": encoder_masks,
